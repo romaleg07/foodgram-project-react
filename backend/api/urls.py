54483fd2292b3321b933e33c32f02fc1d2bfddf8
romaleg07 <romaleg.sky@yandex.ru>
2023-03-05 21:28:47 +0400

    transef users and recipes views in api app. Added validation in serializer

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import IngredientViewSet, RecipeViewSet, TagsViewSet
-from .views import MeViewSet, SetNewPasswordUser, UsersViewSet
+from .views import (IngredientViewSet, MeViewSet, RecipeViewSet,
+                    SetNewPasswordUser, TagsViewSet, UsersViewSet)
 
 router = routers.DefaultRouter()
 router.register(r'users', UsersViewSet, basename='users')
