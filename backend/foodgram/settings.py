3d1e5d0b86e5c7fab21371cce104ad871d5f9788
romaleg07 <romaleg.sky@yandex.ru>
2023-03-05 21:26:10 +0400

    transef users and recipes views in api app. Added validation in serializer

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,7 +1,6 @@
 import os
-from pathlib import Path
-
 from dotenv import load_dotenv
+from pathlib import Path
 
 load_dotenv()
 
@@ -73,28 +72,6 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv(
-#             'DB_ENGINE', default='django.db.backends.postgresql'),
-#         'NAME': os.getenv(
-#             'DB_NAME',
-#             default='foodgram'),
-#         'USER': os.getenv(
-#             'POSTGRES_USER',
-#             default='foodgram'),
-#         'PASSWORD': os.getenv(
-#             'POSTGRES_PASSWORD',
-#             default='123456'),
-#         'HOST': os.getenv(
-#             'DB_HOST',
-#             default='db'),
-#         'PORT': os.getenv(
-#             'DB_PORT',
-#             default='5432'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': os.getenv(
@@ -116,13 +93,6 @@ DATABASES = {
             default='5432'),
     }
 }
-
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
 
 
 AUTH_USER_MODEL = 'users.User'
